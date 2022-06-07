4fdaa34e56b0d2796f168d49648173d972004939
Emilio Bresolin <emiliotdm@hotmail.com>
2022-06-07 18:51:59 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App using C#.";
+            ViewData["Developer"] = "Emilio Bresolin";
 
             return View();
         }
